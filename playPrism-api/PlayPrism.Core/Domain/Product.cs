58149ff83fd141a51dc86cf446ae6ef29b1aa64b
Arsenii <arsenii.vm@gmail.com>
2023-04-26 15:44:54 +0300

    added documentation to Domain Models and updated PlayPrismContext.cs

--- playPrism-api/PlayPrism.Core/Domain/Product.cs
+++ playPrism-api/PlayPrism.Core/Domain/Product.cs
@@ -1,20 +1,52 @@
 ﻿namespace PlayPrism.Core.Domain;
 
+/// <summary>
+/// Entity that represents table Product in database.
+/// </summary>
 public class Product : BaseEntity
 {
+    /// <summary>
+    /// Gets or sets Name of Product.
+    /// </summary>
     public string Name { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets Description of Product.
+    /// </summary>
     public string Description { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets Image url of Product.
+    /// </summary>
     public string Image { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets Price of Product.
+    /// </summary>
     public decimal Price { get; set; }
-    
-    public IEnumerable<ProductCategory> ProductCategories { get; set; }
-    
-    public IEnumerable<ProductItem> ProductItems { get; set; }
-    
-    public IEnumerable<UserReview> UserReviews { get; set; }
-    
-    public IEnumerable<ProductConfiguration> ProductConfigurations { get; set; }
+
+    /// <summary>
+    /// Gets or sets FK id to ProductCategory entity.
+    /// </summary>
+    public Guid ProductCategoryId { get; set; }
+
+    /// <summary>
+    /// Gets or sets reference to ProductCategory entity.
+    /// </summary>
+    public ProductCategory ProductCategory { get; set; }
+
+    /// <summary>
+    /// Gets or sets references to ProductItems entities.
+    /// </summary>
+    public IList<ProductItem> ProductItems { get; set; }
+
+    /// <summary>
+    /// Gets or sets references to UserReviews entities.
+    /// </summary>
+    public IList<UserReview> UserReviews { get; set; }
+
+    /// <summary>
+    /// Gets or sets references to ProductConfigurations entities.
+    /// </summary>
+    public IList<ProductConfiguration> ProductConfigurations { get; set; }
 }
--- playPrism-api/PlayPrism.Core/Domain/ProductConfiguration.cs
+++ playPrism-api/PlayPrism.Core/Domain/ProductConfiguration.cs
@@ -1,12 +1,32 @@
 ﻿namespace PlayPrism.Core.Domain;
 
+/// <summary>
+/// Entity that represents table ProductConfiguration in database.
+/// </summary>
 public class ProductConfiguration : BaseEntity
 {
+    /// <summary>
+    /// Gets or sets ConfigurationName.
+    /// </summary>
+    public string ConfigurationName { get; set; }
+
+    /// <summary>
+    /// Gets or sets FK id to Product entity.
+    /// </summary>
     public Guid ProductId { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets reference to Product entity.
+    /// </summary>
     public Product Product { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets FK id to VariationOption entity.
+    /// </summary>
     public Guid VariationOptionId { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets reference to Product entity.
+    /// </summary>
     public VariationOption VariationOption { get; set; }
 }
