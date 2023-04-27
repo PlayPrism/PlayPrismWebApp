70143a5ccabd7f2d0eba4435e59df2f18a5f5994
wher3ver <dddaaannn1322@gmail.com>
2023-04-28 00:04:14 +0300

    Added logging with Serilog, changed Serilog configuration and installed Nuget-packages for enrichers

--- playPrism-api/PlayPrism.API/Program.cs
+++ playPrism-api/PlayPrism.API/Program.cs
@@ -1,17 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using PlayPrism.DAL;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PlayPrismContext>(x =>
     x.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+
+builder.Host.UseSerilog();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
 
 var app = builder.Build();
 
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
