8d0475fbae1e425480117ddf7912dec6ba893072
david.dzabakhidze <david.dzabakhidze@hys-enterprise.com>
2023-05-15 11:07:38 +0300

    Added product details card component

---
+++ playPrism-web/src/modules/products/components/product-details/product-details-card/product-details-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-card',
+  templateUrl: './product-details-card.component.html',
+  styleUrls: ['./product-details-card.component.scss']
+})
+export class ProductDetailsCardComponent {
+
+}
--- playPrism-web/src/modules/products/products.module.ts
+++ playPrism-web/src/modules/products/products.module.ts
@@ -5,9 +5,10 @@ import { ProductsService } from '../../core/services';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { ProductDetailsCardComponent } from './components/product-details/product-details-card/product-details-card.component';
 
 @NgModule({
-  declarations: [HomeComponent, ProductDetailsComponent, ProductCartComponent],
+  declarations: [HomeComponent, ProductDetailsComponent, ProductCartComponent, ProductDetailsCardComponent],
   imports: [ProductsRoutingModule, SharedModule],
   providers: [ProductsService],
 })
