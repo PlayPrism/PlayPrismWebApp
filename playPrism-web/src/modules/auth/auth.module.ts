f6036301097c22e254402109439d050a1a81ddc2
vanyavidivici <vanyavidivici@gmail.com>
2023-05-18 01:02:37 +0300

    add auth module routing and correct path mistakes

--- playPrism-web/src/modules/auth/auth.module.ts
+++ playPrism-web/src/modules/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AuthRoutingModule } from "./auth-routing.module";
 import { SharedModule } from "../shared/shared.module";
