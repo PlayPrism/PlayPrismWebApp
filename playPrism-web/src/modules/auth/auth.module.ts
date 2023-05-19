9208c8b378cda374c8cdc5a560a6679ce6488bc9
vanyavidivici <vanyavidivici@gmail.com>
2023-05-19 17:03:48 +0300

    add first and unfinished layout of sign-up page

--- playPrism-web/src/modules/auth/auth.module.ts
+++ playPrism-web/src/modules/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AuthRoutingModule } from "./auth-routing.module";
 import { SharedModule } from "../shared/shared.module";
+import {FormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [SignUpComponent],
-  imports: [AuthRoutingModule, SharedModule]
+    imports: [AuthRoutingModule, SharedModule, FormsModule]
 })
 export class AuthModule { }
