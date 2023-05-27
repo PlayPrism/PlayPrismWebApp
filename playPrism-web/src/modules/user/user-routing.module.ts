f66ba3e3e60290849d3a7bc432ce7328f9c4f9d4
vanyavidivici <vanyavidivici@gmail.com>
2023-05-27 21:03:59 +0300

    made purchase-history full layout

--- playPrism-web/src/modules/user/user-routing.module.ts
+++ playPrism-web/src/modules/user/user-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { Route, Routes, RouterModule } from '@angular/router';
 import { ShellComponent } from '../shared/components';
 import { ProfileComponent } from './components/profile/profile.component';
+import {PurchaseHistoryComponent} from "./components/purchase-history/purchase-history.component";
 
 export const usersRoute: Route = {
   path: '',
   component: ShellComponent,
-  children: [{ path: '', component: ProfileComponent }],
+  children: [{ path: '', component: ProfileComponent },
+    {path: 'history', component: PurchaseHistoryComponent}],
 };
 
 const routes: Routes = [usersRoute];
