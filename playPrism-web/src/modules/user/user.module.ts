ef532201bc540298f6fcadfbacd75fd55a56f13c
vanyavidivici <vanyavidivici@gmail.com>
2023-05-27 22:45:43 +0300

    fixed some problems with purchase-history component and added user-listgroup component

--- playPrism-web/src/modules/user/user.module.ts
+++ playPrism-web/src/modules/user/user.module.ts
@@ -4,9 +4,10 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { UserService } from 'src/core/services';
 import { SharedModule } from '../shared/shared.module';
 import { PurchaseHistoryComponent } from './components/purchase-history/purchase-history.component';
+import { UserListgroupComponent } from './components/user-listgroup/user-listgroup.component';
 
 @NgModule({
-  declarations: [ProfileComponent, PurchaseHistoryComponent],
+  declarations: [ProfileComponent, PurchaseHistoryComponent, UserListgroupComponent],
   imports: [UserRoutingModule, SharedModule],
   providers: [UserService],
 })
