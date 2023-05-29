f9d25c0c6064b448b7cdff461af79afc9f5ce34f
david.dzabakhidze <david.dzabakhidze@hys-enterprise.com>
2023-05-29 07:41:02 +0300

    Added login method in service

--- playPrism-web/src/core/services/account.service.ts
+++ playPrism-web/src/core/services/account.service.ts
@@ -2,19 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
-import {
-  Product,
-  SearchItem,
-  ApiResponse,
-  Auth,
-  RegistrationRequest,
-} from '../models';
+import { Auth, RegistrationRequest } from '../models';
 import { BaseService } from './base.service';
+
+export const tokenStorageKey = 'auth';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService extends BaseService<Auth> {
+export class AccountService extends BaseService<Auth> {
   private readonly baseUrl = environment.apiUrl + '/account';
 
   constructor(http: HttpClient) {
@@ -22,6 +18,14 @@ export class AuthService extends BaseService<Auth> {
   }
 
   public register(body: RegistrationRequest): Observable<Auth> {
-    return this.http.post<Auth>(`${this.baseUrl}/register`, body);
+    return this.create(`${this.baseUrl}/register`, body).pipe(
+      map((response) => response.data)
+    );
+  }
+
+  public login(body: RegistrationRequest): Observable<Auth> {
+    return this.create(`${this.baseUrl}/login`, body).pipe(
+      map((response) => response.data)
+    );
   }
 }
--- playPrism-web/src/modules/auth/components/sign-up/sign-up.component.ts
+++ playPrism-web/src/modules/auth/components/sign-up/sign-up.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import {HeaderComponent} from "../../../shared/components";
+import { HeaderComponent } from '../../../shared/components';
 
 import { FormBuilder, Validators } from '@angular/forms';
 import { catchError, of, tap } from 'rxjs';
-import { AuthService } from 'src/core/services';
-
+import { AccountService, tokenStorageKey } from 'src/core/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -24,7 +24,8 @@ export class SignUpComponent {
 
   constructor(
     private readonly fb: FormBuilder,
-    private readonly authService: AuthService
+    private readonly accountService: AccountService,
+    private readonly router: Router
   ) {}
 
   public get emailControl() {
@@ -41,7 +42,7 @@ export class SignUpComponent {
 
   onSubmit(): void {
     this.isLoading = true;
-    this.authService
+    this.accountService
       .register({
         email: this.emailControl.value!,
         password: this.passwordControl.value!,
@@ -54,9 +55,13 @@ export class SignUpComponent {
           }
           return of(null);
         }),
-        tap(() => {
-          this.isLoading = false;
-        })
+        tap((authObject) => {
+          if (authObject?.accessToken) {
+            localStorage.setItem(tokenStorageKey, JSON.stringify(authObject));
+            this.router.navigate(['']);
+          }
+        }),
+        tap(() => (this.isLoading = false))
       )
       .subscribe();
   }
